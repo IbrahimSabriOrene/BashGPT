be4b13682282bc34c6c0926ea69b2c2db7c0f11b
İbrahim Sabri Örene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-22 16:42:51 +0300

    Update bashgpt.sh
    Changed the test

--- bashgpt.sh
+++ bashgpt.sh
@@ -6,22 +6,28 @@ OPENAI_API_KEY="your_api_key_here"
 # The model you want to use
 MODEL_NAME="gpt-3.5-turbo"
 
-# The prompt message
-USER_MESSAGE="Say this is a test!"
+# Function to make the API request
+make_api_request() {
+  local user_message="$1"
+  
+  # Make the API request using curl
+  API_RESPONSE=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -d '{
+       "model": "'"${MODEL_NAME}"'",
+       "messages": [{"role": "system", "content": "You are a helpful assistant that provides information for linux commands, returning the code only."}, {"role": "user", "content": "'"${user_message}"'"}],
+       "temperature": 0.7
+     }' "https://api.openai.com/v1/chat/completions")
+}
 
-# Make the API request using curl
-API_RESPONSE=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "'"${MODEL_NAME}"'",
-     "messages": [{"role": "user", "content": "'"${USER_MESSAGE}"'"}],
-     "temperature": 0.7
-   }' "https://api.openai.com/v1/chat/completions")
+# Get user's question
+echo "Ask a question:"
+read USER_QUESTION
+
+# Make the API request with the user's question
+make_api_request "$USER_QUESTION"
 
 # Check if the request was successful
 if [ $? -eq 0 ]; then
-  echo "API Response:"
-  echo "$API_RESPONSE"
-
   # Check if the response is not null or empty
   if [ -n "$API_RESPONSE" ]; then
     # Extract and print the assistant's response
@@ -34,5 +40,3 @@ if [ $? -eq 0 ]; then
 else
   echo "Failed to retrieve data from the OpenAI API."
 fi
-
-
